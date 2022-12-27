0938254638aa016140d96c587bf171bbe5c87238
Z0ro0 <chlrkdmf0805@gmail.com>
2022-12-27 09:51:17 +0900

    필터링 이용해서 이름에 신 들어간 사람만 뽑아내기(설명)

--- src/FilteringExample.java
+++ src/FilteringExample.java
@@ -5,13 +5,13 @@ public class FilteringExample {
     public static void main(String[] args){
         List<String> names = Arrays.asList("홍길동", "신용권", "김자바", "신용권", "신민철");
 
-        names.stream()
-                .distinct()
-                .forEach(n -> System.out.println(n));
+        names.stream()//오리지널 스트림 변환
+                .distinct()//중간 처리 ==> 중복 제거
+                .forEach(n -> System.out.println(n));// 최종 처리
         System.out.println();
 
         names.stream()
-                .filter(n -> n.startsWith("신"))
+                .filter(n -> n.startsWith("신"))//중간 처리
                 .forEach(n -> System.out.println(n));
     }
 }
